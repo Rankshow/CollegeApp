b9c747171b26d24b7f4451bd50cb728839c5b15b
rankshow <uwaomaobinna20@gmail.com>
2023-09-12 19:37:37 +0100

    added httpPut request

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -85,7 +85,7 @@ namespace CollegeApp.Controllers
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public ActionResult<StudentDTO> CreateStudent([FromBody]StudentDTO model)
+        public ActionResult<StudentDTO> CreateStudent([FromBody] StudentDTO model)
         {
             
             // if (!ModelState.IsValid)
@@ -99,7 +99,7 @@ namespace CollegeApp.Controllers
             //     ModelState.AddModelError("AdmissionDate Error", "Admission data must be greater than or eqaaul to today date");
             //     return BadRequest(ModelState);
             // }
-
+ 
             int newId = CollegeRepository.Students.LastOrDefault().Id + 1;
             Student student = new Student
             {
@@ -117,6 +117,31 @@ namespace CollegeApp.Controllers
             return CreatedAtRoute("GetStudentById", new { id = model.Id }, model);
         }
        
+        //  * This will update the record
+        [HttpPut]
+        [Route("Update")] 
+        //api/student/update
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult UpdateStudent([FromBody] StudentDTO model)   
+        {
+            if (model == null || model.Id <= 0)
+             return BadRequest();
+
+            var existingStudent = CollegeRepository.Students.Where(s => s.Id == model.Id).FirstOrDefault();
+
+            if (existingStudent == null)
+            return NotFound();
+
+            existingStudent.StudentName =  model.StudentName;
+            existingStudent.Email = model.Email;
+            existingStudent.Address = model.Address;
+
+            return NoContent();
+        }
+
         [HttpDelete("{id}", Name = "DeleteStudentById")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
